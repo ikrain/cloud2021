b90be5c97720184f2ad95db2b4f10089f7751032
ccstill <17634539621@163.com>
2021-02-03 18:20:16 +0800

    update

---
+++ 10-cloud-consumerconsul-order80/src/main/java/cn/krain/springcloud/OrderConsulMain80.java
@@ -0,0 +1,19 @@
+package cn.krain.springcloud;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author CC
+ * @data 2021/2/3 - 17:40
+ */
+@SpringBootApplication(exclude =  {DataSourceAutoConfiguration.class})
+@EnableDiscoveryClient
+public class OrderConsulMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderConsulMain80.class,args);
+    }
+}
---
+++ 10-cloud-consumerconsul-order80/src/main/java/cn/krain/springcloud/controller/OrderConsulController.java
@@ -0,0 +1,29 @@
+package cn.krain.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+/**
+ * @author CC
+ * @data 2021/2/1 - 23:38
+ */
+@RestController
+@Slf4j
+public class OrderConsulController {
+
+    public static final String INVOKE_URL = "http://consul-provider-payment";
+
+    @Resource
+    private RestTemplate restTemplate;          // 与RedisTemplate性质相同
+
+    @GetMapping(value = "/consumer/payment/consul")
+    public String paymentConsul(){
+        String result = restTemplate.getForObject(INVOKE_URL+"/payment/consul",String.class);
+        return result;
+    }
+}
