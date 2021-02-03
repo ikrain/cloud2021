21e1d25002d4696a6a4ecc8ab341b8fc4968bc67
ccstill <17634539621@163.com>
2021-02-03 17:35:00 +0800

    update

---
+++ 09-cloud-providerconsul-payment8006/src/main/java/cn/krain/springcloud/PaymentMain8006.java
@@ -0,0 +1,17 @@
+package cn.krain.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author CC
+ * @data 2021/2/3 - 17:11
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain8006 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8006.class,args);
+    }
+}
---
+++ 09-cloud-providerconsul-payment8006/src/main/java/cn/krain/springcloud/controller/PaymentController.java
@@ -0,0 +1,24 @@
+package cn.krain.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * @author CC
+ * @data 2021/2/3 - 17:12
+ */
+@RestController
+@Slf4j
+public class PaymentController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @RequestMapping(value = "/payment/consul")
+    public String paymentConsul(){
+        return "spring cloud with consul:"+serverPort+"\t"+ UUID.randomUUID().toString();
+    }
+}
