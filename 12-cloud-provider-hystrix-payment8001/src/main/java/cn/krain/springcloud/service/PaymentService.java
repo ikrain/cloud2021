c2d5f2614e4cd2e5cc357e89062dbdfc54511d40
ccstill <17634539621@163.com>
2021-02-26 16:21:20 +0800

    update

--- 12-cloud-provider-hystrix-payment8001/src/main/java/cn/krain/springcloud/service/PaymentService.java
+++ 12-cloud-provider-hystrix-payment8001/src/main/java/cn/krain/springcloud/service/PaymentService.java
@@ -1,8 +1,10 @@
 package cn.krain.springcloud.service;
 
+import cn.hutool.core.util.IdUtil;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.concurrent.TimeUnit;
 
@@ -18,7 +20,8 @@ public class PaymentService {
     }
 
     /**
-     * @HystrixCommand： 该注解为当前服务做服务降级，当请求超时或发生异常时，自动转入到paymentInfo_TimeoutHandler服务，并返回执行结果
+     * @HystrixCommand： 该注解为当前服务做服务降级，当请求超时或发生异常时，
+     *                   自动转入到paymentInfo_TimeoutHandler服务，并返回执行结果
      * @param id
      * @return
      */
@@ -36,5 +39,24 @@ public class PaymentService {
     }
     public String paymentInfo_TimeoutHandler(Integer id){
         return "线程池："+Thread.currentThread().getName()+"  paymentInfo_TimeoutHandler， id："+ id;
+    }
+
+    // 服务熔断
+    @HystrixCommand(fallbackMethod = "paymentCircuitBreaker_Fallback", commandProperties = {
+            @HystrixProperty(name = "circuitBreaker.enabled",value = "true"),
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold",value = "10"),
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds",value = "10000"),
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage",value = "60")
+    })
+    public String paymentCircuitBreaker(@PathVariable("id") Integer id){
+        if ( id<0 ){
+            throw new RuntimeException("...id不能为负数");
+        }
+        String serialNumber = IdUtil.simpleUUID();
+        return Thread.currentThread().getName()+"\t"+"调用成功，流水号："+serialNumber;
+    }
+
+    public String paymentCircuitBreaker_Fallback(@PathVariable("id") Integer id){
+        return "id不能为负数，请稍后再试...id:"+id;
     }
 }
---
+++ 14-cloud-consumer-hystrix-dashboard9001/src/main/java/cn/krain/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,20 @@
+package cn.krain.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author CC
+ * @data 2021/2/26 - 16:11
+ *
+ * Hystrix服务监控hystrixDashboard
+ *
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class,args);
+    }
+}
